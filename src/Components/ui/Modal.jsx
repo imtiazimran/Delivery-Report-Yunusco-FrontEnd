173e8c01afb36e4055c43578ae05acc2c11fca67
Imran <imtiazimran@outlook.com>
2023-09-04 13:41:07 +0600

    partial job api added on the mmodal

--- src/Components/ui/Modal.jsx
+++ src/Components/ui/Modal.jsx
@@ -8,12 +8,13 @@ export default function PD_Modal({ isOpen, setIsOpen, selectedJobForPartialDeliv
     setIsOpen(false)
   }
 
+  
   const handlePartialDelivery = async () => {
 
     if (selectedJobForPartialDelivery && partialDeliveryQty > 0) {
       try {
         const response = await axios.put(
-          `http://localhost:8570/updatePartialDelivery/${selectedJobForPartialDelivery._id}`,
+          `https://delivery-report-yunusco-back-end.vercel.app/updatePartialDelivery/${selectedJobForPartialDelivery._id}`,
           { partialDeliveryQty }
         );
 
@@ -93,6 +94,14 @@ export default function PD_Modal({ isOpen, setIsOpen, selectedJobForPartialDeliv
                     >
                       Submit
                     </button>
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      Cancel
+                    </button>
+                    
                   </div>
                 </Dialog.Panel>
               </Transition.Child>
