8774c43dafdda4acd133eb306cf4f68da2eb51f6
Imran <imtiazimran@outlook.com>
2023-09-11 02:11:59 +0600

    some icon added

--- src/Components/TotalDelivery.jsx
+++ src/Components/TotalDelivery.jsx
@@ -88,10 +88,14 @@ const TotalDelivery = () => {
     return (
         <div>
             <div className="text-2xl py-3 bg-sky-700 text-white text-center">Total Delivery  </div>
-            <input onChange={(e) => setSearchQuery(e.target.value)}
-                type="number"
-                placeholder="Search By JOB"
-                className="input input-bordered input-primary w-4/5 mx-auto block my-5 max-w-xs text-center" />
+            <div className="form-control">
+                <div className="input-group input-group-sm justify-center my-5">
+                    <input  onChange={(e) => setSearchQuery(e.target.value)} type="text" placeholder="Search…" className="input input-sm input-bordered" />
+                    <button className="btn btn-sm btn-square">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                    </button>
+                </div>
+            </div>
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
